c048a022d6569e61af58dbadfa5e1555b728d357
Alex Sanudo <alejosanudo@Alexs-MacBook-Pro.local>
2015-09-08 15:41:23 -0700

    Added friends list column to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150908192353) do
+ActiveRecord::Schema.define(version: 20150908224055) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20150908192353) do
     t.datetime "oauth_expires_at"
     t.string   "provider"
     t.string   "uid"
+    t.string   "friends_list"
   end
 
 end
