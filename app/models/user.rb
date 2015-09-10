35a6fed302b461d6b4c01d2e0d630446eaa8df2b
Alex Sanudo <alejosanudo@Alexs-MacBook-Pro.local>
2015-09-09 19:00:20 -0700

    Friendships are now created properly

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,22 +7,31 @@ class User < ActiveRecord::Base
   def self.from_omniauth(auth)
     @graph = Koala::Facebook::API.new(auth.credentials.token)
     user = User.find_by(uid: auth.uid)
-    friends  =[]
-    names = []
-    pics = []
-    # result = @graph.get_connections("me", "taggable_friends")
-    # while result 
-    #   friends << result
-    #   result = result.next_page
-    # end
-    # friends.flatten!
-    # friends.length.times do |i|
-    #   names << friends[i]['name']
-    #   pics << friends[i]['picture']['data']['url']
-    # end
     if !user    
       user = User.create(username: auth.info.name, uid: auth.uid, email: @graph.get_object("me?fields=name,picture,email")['email'])
     end
+    friends  =[]
+    names = []
+    pics = []
+    result = @graph.get_connections("me", "taggable_friends")
+    while result 
+      friends << result
+      result = result.next_page
+    end
+    friends.flatten!
+    friends.length.times do |i|
+      names << friends[i]['name']
+      pics << friends[i]['picture']['data']['url']
+    end
+    names.each do |name|
+      friend_user = User.find_by username: name
+      if friend_user
+        friendship = Friendship.create(user_id: user.id, homiie_id: friend_user.id)
+        friendship.save!
+      end
+    end
+
+    
     user.provider = auth.provider
     user.oauth_token = auth.credentials.token
     user.oauth_expires_at = Time.at(auth.credentials.expires_at)
